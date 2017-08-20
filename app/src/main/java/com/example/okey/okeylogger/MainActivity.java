0fdd6750dff239c5e1c8a996409fa92f91226cf0
aarbanas <aarbanas@riteh.hr>
2017-08-20 21:09:08 +0200

    Android permission is now asked on startup

--- app/src/main/java/com/example/okey/okeylogger/MainActivity.java
+++ app/src/main/java/com/example/okey/okeylogger/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.okey.okeylogger;
 
+import android.Manifest;
 import android.app.DialogFragment;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.CharArrayBuffer;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,6 +18,8 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.provider.DocumentsContract;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -154,6 +158,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         // so the user isn't able to edit the phrase
         readPhrase.setActivated(false);
         readPhrase.setFocusable(false);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
 
         // setting touch listener to handle touch events
         readPhrase.setOnTouchListener(this);
@@ -777,6 +785,33 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             handler.postDelayed(this, 0);
         }
     };
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
 }
 
